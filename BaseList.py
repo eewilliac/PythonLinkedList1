0fe85cba9f3970eaf28b774feaeb7a0801f29cac
eewilliac <eewilliac@gmail.com>
2017-05-30 16:41:01 -0400

    Initial Commit, continue work on BaseList.py

---
+++ BaseList.py
@@ -0,0 +1,3 @@
+from Node import _Node
+class _BaseList:
+    def __init__(self):
---
+++ Node.py
@@ -0,0 +1,5 @@
+class _Node:
+    def __init__(self,data,prev,next):
+        self._data = data
+        self._prev = prev
+        self._next = next
